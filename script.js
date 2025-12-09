6963ea61403a245947ab6ef28c29e7de901342c8
devlele <alefefreitassantos@gmail.com>
2025-12-09 13:58:16 -0300

    closes #3 - implementação do botão somar, com javascript

---
+++ script.js
@@ -0,0 +1,9 @@
+function calc(){
+    var input1 = parseFloat(document.getElementById("input1").value);
+    var input2 = parseFloat(document.getElementById("input2").value);
+
+    var output = input1 + input2;
+    document.getElementById("output").value = output;
+
+
+}
