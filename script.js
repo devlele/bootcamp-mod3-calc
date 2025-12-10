d593500b7218465550319408cffeb138ee64ead1
unknown <alefefreitassantos@gmail.com>
2025-12-10 13:22:39 -0300

    closes #4 - implementação dos 4 botoes de operação

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
-function calc(){
+function calc(operation){
     var input1 = parseFloat(document.getElementById("input1").value);
     var input2 = parseFloat(document.getElementById("input2").value);
 
-    var output = input1 + input2;
+    switch(operation){
+        case '+':
+            var output = input1 + input2;
+            break;
+        
+        case '-':
+            var output = input1 - input2;
+            break;
+
+        case '*':
+            var output = input1 * input2;
+            break;
+
+        case '/':
+            var output = input1 / input2;
+            break;
+    }
+
+
+
     document.getElementById("output").value = output;
 
 
